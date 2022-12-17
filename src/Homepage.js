971f1c0689588cc871f9f9e48f8236d876224195
Bryan Han <bryan.han77@gmail.com>
2022-12-17 01:36:37 -0600

    done

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,18 +1,47 @@
 import React from 'react';
 // import './Homepage.css';
 
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import { firebaseConnect, isLoaded } from 'react-redux-firebase';
+import { connect } from 'react-redux';
+import { compose } from 'redux';
 
 class Homepage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+    
     render() {
+        if (!isLoaded(this.props.homepage)) {
+            return <div>Loading...</div>;
+        }
+
+        const decks = Object.keys(this.props.homepage).map(deckId => {
+            const deck = this.props.homepage[deckId];
+            return (
+                <div key={deckId}>
+                    <Link to={`/viewer/${deckId}`}>{deck.name}</Link>
+                </div>
+            );
+        });
+
         return (
             <div>
-                <Link to="/editor">Go to card editor</Link>
-                <hr></hr>
-                <Link to="/viewer">Go to card viewer</Link>
+                <h2>Homepage</h2>
+                <Link to="/editor">Create a new card deck</Link>
+                <h3>Flashcards</h3>
+                { decks }
             </div>
         );
     }
 }
 
-export default Homepage
+const mapStateToProps = state => {
+    return { homepage: state.firebase.data.homepage };
+}
+
+export default compose(
+    firebaseConnect(['/homepage']),
+    connect(mapStateToProps),
+)(Homepage);
